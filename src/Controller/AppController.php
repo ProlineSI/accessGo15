bd57ca0339e76417f4b7602538220d1c0f2190d7
Emiliano Larrea <emiliano18796@hotmail.com>
2019-04-17 19:54:47 -0300

    12345

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -28,7 +28,7 @@ class AppController extends Controller
 {
     public function beforeRender(Event $event) 
     {
-        $this->viewBuilder()->setLayout('default');
+        //$this->viewBuilder()->setLayout('default');
     } 
 
     /**
